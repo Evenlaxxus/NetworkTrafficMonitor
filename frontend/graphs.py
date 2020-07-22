a090ed23ba1356713431db4b2ef10c0fcafd229f
Ania <anula.le@wp.pl>
2020-07-22 12:20:48 +0200

    example graph

--- frontend/graphs.py
+++ frontend/graphs.py
@@ -10,13 +10,20 @@ def sniffing():
 
 def graf():
     dot = Digraph(comment='The Round Table')
-    dot.attr('node', image="switch.png")
+    #dot.attr('node', image="../../images/switch.png")
     dot.attr('node', shape='plaintext')
     dot.attr('node', arrowhead='none')
-    dot.node('A', '192.168.0.1')
-    dot.node('B', '192.168.0.2')
-    dot.node('L', '192.168.0.3')
-    dot.edges(['AB', 'AL'])
-    dot.edge('B', 'L', constraint='false')
+    dot.node('A', 'Modem 192.168.0.1', image='../../images/modem.png')
+    dot.node('B', 'Router 192.168.0.2', image='../../images/router.png')
+    dot.node('C', 'Computer 192.168.0.3', image='../../images/computer.png')
+    dot.node('D', 'Computer 192.168.0.4', image='../../images/computer.png')
+    dot.node('E', 'Router 192.168.0.5', image='../../images/router.png')
+    dot.node('F', 'Computer 192.168.0.6', image='../../images/computer.png')
+    dot.node('G', 'Computer 192.168.0.7', image='../../images/computer.png')
+    dot.edges(['AB', 'AE', 'BC', 'BD', 'EF', 'EG'])
     print(dot.source)
-    dot.render('test-output/round-table.gv', view=True)
+    dot.render('test-output/graph.gv', view=True)
+
+
+if __name__ == "__main__":
+    graf()
